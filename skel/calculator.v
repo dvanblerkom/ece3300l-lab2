62bb19d92061f9f81aa05213f4ec4a8d373e27e6
Daniel Van Blerkom <98125839+dvanblerkom@users.noreply.github.com>
2025-02-08 10:29:34 -0800

    Update calculator.v

--- skel/calculator.v
+++ skel/calculator.v
@@ -8,12 +8,11 @@ module calculator (
    wire				cout, ovf;  // carry_out and overflow
    wire [3:0]			outa;  // adder output
    wire [7:0]			outm;  // multiplier output
-      
-   addsub4 u1 (.A(A), .B(B), .subsel(OP[0]), .X(outa), .cout(cout), .ovf(ovf));
 
-   mult4 u2 (.A(A), .B(B), .X(outm));
-
-   mux10 u3 (.in0({ovf, cout, 4'b0, outa}), .in1({2'b0, outm}), .sel(OP[1]), .out(out));
-
+//
+// make instances of the three modules addsub4, mult4, and mux10
+// and wire them up to create the functionality required.
+//
+	
 endmodule // calculator
 
